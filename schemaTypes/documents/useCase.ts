121a8ac6b3b1a0c4739f2bacd21af4436ea9e6dd
Haji490 <99gkwldnjs99@gmail.com>
2026-03-06 12:13:50 +0900

    useCase_상세설명 삭제 및 name변경

--- schemaTypes/documents/useCase.ts
+++ schemaTypes/documents/useCase.ts
@@ -5,23 +5,28 @@ export default defineType ({
     title: '적용 사례',
     type: 'document',
     fields: [
-        {
+        defineField({
             name: 'title',
             title: '제목',
             type: 'string',
-        },
-        {
+        }),
+        defineField({
             name: 'slug',
             title: 'Slug',
             type: 'slug',
             options: {source: 'title'},
-        },
-        {
+        }),
+        defineField({
             name: 'summary',
             title: '요약',
             type: 'text'
-        },
-        {
+        }),
+        defineField({
+            name: 'description',
+            title: '사례 설명',
+            type: 'text'
+        }),
+        defineField({
             name: 'systems',
             title: '관련 시스템',
             type: 'array',
@@ -29,8 +34,8 @@ export default defineType ({
                 type: 'reference',
                 to:[{type: 'system'}]
             }]
-        },
-        {
+        }),
+        defineField({
             name: 'industries',
             title: '산업',
             type: 'array',
@@ -38,28 +43,22 @@ export default defineType ({
                 type: 'reference',
                 to:[{type: 'industry'}]
             }]
-        },
-        {
-            name: 'content',
-            title: '상세 내용',
-            type: 'array',
-            of: [{type: 'block'}]
-        },
-        {
+        }),
+        defineField({
             name: 'mainImage',
             title: '대표 이미지',
             type: 'image',
             options: {hotspot: true}
-        },
-        {
+        }),
+        defineField({
             name: 'startDate',
             title: '시작일',
             type: 'date'
-        },
-        {
+        }),
+        defineField({
             name: 'endDate',
             title: '종료일',
             type: 'date'
-        },
+        }),
     ]
 })
